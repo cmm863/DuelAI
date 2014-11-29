8ce2b4b8f79f5e783494411f99d0fe69a6fc56b3
Connor McBride <cmm863@mst.edu>
2014-11-29 03:22:37 -0600

    xD My function definitions went rogue.

--- gamestatehandler.h
+++ gamestatehandler.h
@@ -13,24 +13,4 @@ public:
   static void SerializeGameState(vector<Tile> map, vector<Unit> units, int turn_number, int id_iterator);
 };
 
-template <typename T>
-void ProtoHandler::OverwriteOutputs(const T &proto) {
-  fstream output(file_output, ios::out | ios::trunc | ios::binary);
-  proto.SerializeToOstream(&output);
-  return;
-}
-
-template <typename T>
-void ProtoHandler::AppendToOutputs(const T &proto) {
-  fstream output(file_output, ios::out | ios::ate | ios::binary);
-  proto.SerializeToOstream(&output);
-  return;
-}
-
-template <typename T>
-void ProtoHandler::LoadInputTo(T &proto) {
-  fstream input(file_input, ios::in | ios::binary);
-  proto.ParseFromIstream(&input);
-}
-
 #endif
--- protohandler.h
+++ protohandler.h
@@ -22,4 +22,26 @@ public:
   void LoadInputTo(T &proto);
 };
 
+
+template <typename T>
+void ProtoHandler::OverwriteOutputs(const T &proto) {
+  fstream output(file_output, ios::out | ios::trunc | ios::binary);
+  proto.SerializeToOstream(&output);
+  return;
+}
+
+template <typename T>
+void ProtoHandler::AppendToOutputs(const T &proto) {
+  fstream output(file_output, ios::out | ios::ate | ios::binary);
+  proto.SerializeToOstream(&output);
+  return;
+}
+
+template <typename T>
+void ProtoHandler::LoadInputTo(T &proto) {
+  fstream input(file_input, ios::in | ios::binary);
+  proto.ParseFromIstream(&input);
+}
+
+
 #endif
